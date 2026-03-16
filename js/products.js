7cc7b206ddeb0474df568864eaf94b63340eb870
buypal-store <yazidchahua10@hotmail.com>
2026-03-16 17:04:15 -0500

    Update products.js
    nuevo

--- js/products.js
+++ js/products.js
@@ -664,7 +664,7 @@ const PRODUCTS = [
 {
   id: "set-limpiador-biberones",
   name: "Set Limpiador de Biberones",
-  price: 25,
+  price: 39,
   category: "Higiene y Limpieza",
   subCategory: "Limpiadores",
   type: "limpieza",
